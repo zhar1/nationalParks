c8ee98f066786a2093d372b1ebc24d5df19f5d64
Zaire <zhar1.zh@gmail.com>
2023-07-12 21:48:30 -0500

    add colorSet and home screen imageView

---
+++ nationalPark/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nationalPark
+//
+//  Created by Zaire Harris on 7/10/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
